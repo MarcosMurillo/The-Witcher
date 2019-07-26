3ccbd95114832504a8c48c8321da8d56616f7f61
Marcos Murillo <murillo.felix@ies2.com.br>
2019-07-26 11:10:22 -0300

    Estilizando a pagina incial

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import BackgroundImg from '../../assets/background.png';
-import { Container } from './styles';
+import React from 'react';
+import { Container, Content } from './styles';
+import Logo from '../../assets/logo_dark.png';
 
-class Login extends Component {
-  state = {};
-
-  render() {
-    return (
-      <Container>
-        <img src={BackgroundImg} alt="Backgroung" />
-      </Container>
-    );
-  }
+function Login() {
+  return (
+    <Container>
+      <Content>
+        <img src={Logo} alt="logo" />
+        <input type="text" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+      </Content>
+    </Container>
+  );
 }
 
 export default Login;
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -1,7 +1,39 @@
 import styled from 'styled-components';
+import backgroundImage from '../../assets/background.png';
 
 export const Container = styled.div`
-  flex: 1;
-  background-image: url('../assets/background.png');
+  height: 100vh;
+  width: 100vw;
+  background-image: url(${backgroundImage});
+  background-size: cover;
+  overflow: hidden;
   background-repeat: no-repeat;
+  background-size: cover;
+  background-position: top;
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    background-position-x: -60rem;
+  }
+  @media (max-height: 768px) {
+    background-position-x: -60rem;
+  }
+`;
+export const Content = styled.div`
+  margin: 30vh auto auto auto;
+  display: flex;
+  flex-direction: column;
+
+  img {
+    display: flex;
+    width: 250px;
+  }
+
+  input {
+    margin-top: 10px;
+    border: 0;
+    background-color: transparent;
+  }
 `;
